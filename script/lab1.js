11d12fe88609d57f6b344af4cd9b8c1280849076
zhan2819 <jasmine199910@gmail.com>
2019-04-24 01:18:02 -0400

    filter firebase data

--- script/lab1.js
+++ script/lab1.js
@@ -1,8 +1,9 @@
 // DOM elements
 const tipList = document.querySelector('.tips');
 
-db.collection('tips').get().then((snapshot) => {
+db.collection('tips').where('lab','==','1').get().then((snapshot) => {
     let html = '';
+    if (snapshot.length) {
     snapshot.docs.forEach(doc => {
         const tip = doc.data();
       const li = `
@@ -15,6 +16,23 @@ db.collection('tips').get().then((snapshot) => {
     })
     tipList.innerHTML = html
     console.log(snapshot.docs);
+    }else{
+        let html = '';
+        const li = `
+        <div class="row">
+        <div class="col s12 m6">
+          <div class="card blue-grey darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">;(</span>
+              <h5>No one has posted tips about lab1</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+        `;
+        html += li;
+        tipList.innerHTML = html;
+    }
 })
 
 

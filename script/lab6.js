21ceea517886a760ebc3e41347e754db4efed4eb
zhan2819 <jasmine199910@gmail.com>
2019-04-24 02:26:48 -0400

    lab6 page

---
+++ script/lab6.js
@@ -0,0 +1,36 @@
+// DOM elements
+const tipList = document.querySelector('.tips');
+
+db.collection('tips').where('lab','==','6').get().then((snapshot) => {
+    let html = '';
+    snapshot.docs.forEach(doc => {
+        const tip = doc.data();
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${tip.topic} </div>
+          <div class="collapsible-body white"> ${tip.content} </div>
+        </li>
+      `;
+      html += li;
+    })
+    tipList.innerHTML = html
+    console.log(snapshot.docs);
+    
+})
+
+
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
+
+$(document).ready(function(){
+  $('select').formSelect();
+})

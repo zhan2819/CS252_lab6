a760e2d5bb0bafa7ea27ac6fceeeb22230881a2c
zhan2819 <jasmine199910@gmail.com>
2019-04-25 09:21:11 -0400

    firebase functions

--- functions/index.js
+++ functions/index.js
@@ -3,58 +3,30 @@
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
-// Configure the email transport using the default SMTP transport and a GMail account.
-// For Gmail, enable these:
-// 1. https://www.google.com/settings/security/lesssecureapps
-// 2. https://accounts.google.com/DisplayUnlockCaptcha
-// For other types of transports such as Sendgrid see https://nodemailer.com/transports/
-// TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
-//const gmailEmail = functions.config().gmail.email;
-//const gmailPassword = functions.config().gmail.password;
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'jasmine199910@gmail.com',
-    pass: 'Google#234',
-  },
+const createNotification = ((notification) => {
+    return admin.firestore().collection('notifications')
+      .add(notification)
+      .then(doc => console.log('notification added', doc));
+  });
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return null;
+
+      });
 });
-
-// Your company name to include in the emails
-// TODO: Change this to your app or company name to customize the email sent.
-const APP_NAME = '252Tipz';
-
-// [START sendWelcomeEmail]
-/**
- * Sends a welcome email to new user.
- */
-// [START onCreateTrigger]
-exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
-// [END onCreateTrigger]
-  // [START eventAttributes]
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
-  // [END eventAttributes]
-
-  return sendWelcomeEmail(email, displayName);
-});
-// [END sendWelcomeEmail]
-
-// Sends a welcome email to the given user.
-async function sendWelcomeEmail(email, displayName) {
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email,
-  };
-
-  // The user subscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  await mailTransport.sendMail(mailOptions);
-  console.log('New welcome email sent to:', email);
-  return null;
-}
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Did u finish shell project?");
+  });
